dc7e49d4cf0e9a9850c3236635dba27358687bec
Michael Winkler <michael.winkler@micwin.net>
2012-08-19 00:35:51 +0200

    more documentation, more output

--- webapp/src/main/java/net/micwin/elysium/jobs/AdvancerTask.java
+++ webapp/src/main/java/net/micwin/elysium/jobs/AdvancerTask.java
@@ -50,6 +50,7 @@ import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 public class AdvancerTask extends TimerTask {
 
